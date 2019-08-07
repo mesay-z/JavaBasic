55153581b481754b0efb2cb4796a0b210e3e1fd8
mouloudwissam <archfrimouloud@gmail.com>
2019-08-07 18:48:35 -0400

    add files

---
+++ hello/src/hello/Mysweetprogram.java
@@ -0,0 +1,53 @@
+package hello;
+import java.util.Scanner;
+
+public class Mysweetprogram {
+
+	public static void main(String[] args) {   //method is do something
+      
+		Scanner scanner = new Scanner(System.in);
+		//String x = scanner.nextLine();
+		//System.out.println(x);
+		boolean y =scanner.nextBoolean();
+		//System.out.println(y);
+		//System.out.println(y);
+		//int b = y + 5;
+		//System.out.println(b);
+		
+		  //property is store value
+		
+		//type identifier or variable = new type();//is a structure to create new object}
+     // main method
+    	 //public_ access modifiyer
+    	// static_ key word
+    	// void_ data type
+    	// main_ name of the method
+    // data type
+    	byte number=-128;   //8 bit memory location
+    	byte number2=127;
+    	short a=-32768;
+    	short b=32767;     //16 bit memory location
+    	int x=-2-147-483-648; // 32 bit memory location
+    	int m=2-147-483-647;
+    	long z=3476594865645646L; // 64 bit memory location
+    	double q=1.33d;  // 64 bit memory location
+        float w=1.22222222222f; // 32 bit memory location 
+        char e ='\u00e5';                // 16 bit memory location, 1 char
+        boolean g= false;
+        System.out.println(g);
+	}
+    	//package and variables start with lower cases
+	    //class and project_name start with upper cases
+	//access modifier(who able to access)
+	//1.default=if we do not declear
+	//2.private=the only access wt am declear
+	//3.public=everyboday hv able to access
+	//4.protected=
+  //METHOD static                           non_static
+//         1.void                           1.void
+//         2.return type                    2.return type
+	//when we creat the object of the desired class
+	//desirerd_class_name refvar(object_name)=new(keyword)constructor(instance of the class))
+     //class define the template
+	 //object are specific instance of the template
+
---
+++ hello/src/hello/helloworld.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("blen");
+		//System.out.println("robert@ppn.com");
+		//System.out.println("3255NY");
+		//System.out.println("bible");
+		//System.out.println("robert@ppn.com");
+		String a="USA";
+		String b="2019";
+		System.out.println(a+" "+b );
+	}
+
+}
